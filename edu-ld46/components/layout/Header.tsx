5868c2ce855529c7fb001f27f719732b0febb1f5
thanhsonl2c <thanhsonl2c@gmail.com>
2025-08-14 14:04:40 +0700

    feat: menu

--- edu-ld46/components/layout/Header.tsx
+++ edu-ld46/components/layout/Header.tsx
@@ -1,18 +1,41 @@
 "use client";
 
+import Link from 'next/link';
 import Image from 'next/image'
 
 export default function Header() {
-    return(
-        <div className="header bg-white py-4">
-            <div className="container mx-auto">
+
+    // const [openDropdown, setOpenDropdown] = useState<string | null>(null);
+    // const toggleDropdown = (dropdownName: string) => {
+    //     setOpenDropdown(openDropdown === dropdownName ? null : dropdownName);
+    // };
+
+    return (
+        <div className="header bg-white">
+            <div className="container flex items-center justify-between py-4 mx-auto">
                 <div className="logo">
                     <Image src="/images/logo.png" width={160} height={50} alt="Logo" />
                 </div>
-                <div className="navbar">
+                <div className="navbar relative">
+                    <ul className="flex justify-center lg:gap-4 xl:gap-10 text-base font-semibold text-black">
+                        <li>
+                            <Link href="/" className="transition-all duration-200 hover:text-green-zomp">Trang chủ</Link>
+                        </li>
 
+                        <li>
+                            <Link href="/" className="transition-all duration-200 hover:text-green-zomp">Sản phẩm</Link>
+                        </li>
+
+                        <li>
+                            <Link href="/" className="transition-all duration-200 hover:text-green-zomp">Tin Tức</Link>
+                        </li>
+
+                        <li>
+                            <Link href="/" className="transition-all duration-200 hover:text-green-zomp">Liên hệ</Link>
+                        </li>
+                    </ul>
                 </div>
-                <div className="">
+                <div className="wrapper">
 
                 </div>
             </div>
